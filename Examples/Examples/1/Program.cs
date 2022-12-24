9672a76e7fd5bbec30ad1b497261f3493b6027e5
romanov_daniil <dan-detector@mail.ru>
2022-12-24 14:15:30 +0300

    1 задача, 2 числа

--- Examples/Examples/1/Program.cs
+++ Examples/Examples/1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите сравниваемые числа:");
+    int a=Convert.ToInt32(Console.ReadLine()), 
+        max=0,
+        b=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a);
+Console.WriteLine (b);
+            if (a>b)
+            {
+                max=a;
+            }
+            else max=b;
+Console.WriteLine($"Максимальное число ={max}");           
+            
+
+
+
+
