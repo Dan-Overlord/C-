10ed7a2b0f6f11f195086508ef8a016a85103b77
romanov_daniil <dan-detector@mail.ru>
2022-12-24 14:48:39 +0300

    2 задача, 3 числа

---
+++ Examples/Examples/2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите сравниваемые числа:");
+    int a=Convert.ToInt32(Console.ReadLine()), 
+        max=a,
+        b=Convert.ToInt32(Console.ReadLine()),
+        c=Convert.ToInt32(Console.ReadLine());
+            if (a>b)
+            {
+                max=a;
+            }
+           else if (b>c)
+                {
+                   max=b; 
+                }
+                    else max=c;
+Console.WriteLine($"Максимальное число ={max}");    
