27896c78bc8bf4b4a6bfd380dda2c27f9faa2ffd
romanov_daniil <dan-detector@mail.ru>
2023-02-16 12:15:01 +0300

    8 семинар

---
+++ Examples/Examples/Семинар 8/54/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Console.Clear();
+int [,] array = new int[3, 4];
+FillArrayRandomNumbers(array);
+PrintArray (array);
+Console.WriteLine("Отсортированный массив");
+Change(array);
+PrintArray (array);
+void Change(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
+
---
+++ Examples/Examples/Семинар 8/58/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Console.Clear();
+int [,] first = new int[4, 3];
+int[,] second = new int[3,4];
+int[,] resultMat = new int[4,4];
+
+
+FillArrayRandomNumbers(first);
+FillArrayRandomNumbers(second);
+
+ Console.WriteLine("Первая матрица"); 
+PrintArray (first);
+ Console.WriteLine("Вторая матрица"); 
+PrintArray (second);
+
+
+
+MultiPly(first, second, resultMat);
+
+Console.WriteLine("Результат произведения"); 
+PrintArray (resultMat);
+
+void MultiPly(int[,] first, int[,] second, int[,] resultMat)
+{
+  {
+  for (int i = 0; i < resultMat.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMat.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < first.GetLength(1); k++)
+      {
+        sum += first[i,k] * second[k,j];
+      }
+      resultMat[i,j] = sum;
+    }
+  }
+}
+}
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
