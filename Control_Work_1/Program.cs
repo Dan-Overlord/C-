4db47d7bd63454c8465373fce0e733cbdd033cad
romanov_daniil <dan-detector@mail.ru>
2023-03-01 12:40:19 +0300

    Control_Work_2

--- Control_Work_1/Program.cs
+++ Control_Work_1/Program.cs
@@ -5,6 +5,15 @@ Console.Clear();
 
 string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
-
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
 
 
